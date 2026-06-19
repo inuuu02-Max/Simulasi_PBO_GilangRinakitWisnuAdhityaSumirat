b2a359eb432add6c031f81157c55b81a3a4bbc9b
Gilang <wisnusumirat1@gmail.com>
2026-06-19 11:05:19 +0700

    Tahap 4

---
+++ PendaftaranKedinasan.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'Pendaftaran.php';
+
+class PendaftaranKedinasan extends Pendaftaran {
+    private $skIkatanDinas;
+    private $instansiSponsor;
+
+    public function __construct($id, $nama, $asal, $nilai, $biayaDasar, $sk, $instansi) {
+        parent::__construct($id, $nama, $asal, $nilai, $biayaDasar);
+        $this->skIkatanDinas = $sk;
+        $this->instansiSponsor = $instansi;
+    }
+
+    public function hitungTotalBiaya() {
+        return $this->biayaPendaftaranDasar * 1.25;
+    }
+
+    public function tampilkanInfoJalur() {
+        return "<b>No. SK:</b> " . $this->skIkatanDinas . "<br><b>Instansi:</b> " . $this->instansiSponsor;
+    }
+
+    public static function getDaftarKedinasan($db) {
+        $sql = "SELECT * FROM tabel_pendaftaran WHERE jalur_pendaftaran = 'Kedinasan'";
+        $result = $db->query($sql);
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = new PendaftaranKedinasan($row['id_pendaftaran'], $row['nama_calon'], $row['asal_sekolah'], $row['nilai_ujian'], $row['biaya_pendaftaran_dasar'], $row['sk_ikatan_dinas'], $row['instansi_sponsor']);
+        }
+        return $data;
+    }
+}
